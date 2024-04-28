88cba345a527c165a7f7f94ed7494bc5447e64a5
lukas-577 <lmedinar@utem.cl>
2024-04-28 15:23:24 -0400

    primer commit

---
+++ problema1.js
@@ -0,0 +1,23 @@
+// encontrar la maxima cantidad de rectangulos que se pueden formar con una cantidad de cuadrados
+
+
+
+function maxPanelesEnTecho(a,b,x,y){
+    let areaPanel = a*b;
+    let areaTecho = x*y;
+    if(a&&b>x&&y){
+        return 0;
+    }else if(areaPanel==areaTecho){
+        return 1;
+    }else{
+        return Math.floor(areaTecho/areaPanel);
+    }
+}
+
+console.log("caben:"+maxPanelesEnTecho(1,2,2,4)); 
+
+console.log("caben:"+maxPanelesEnTecho(1,2,3,5));
+console.log("caben:"+maxPanelesEnTecho(2,2,1,10));
+console.log("caben:"+maxPanelesEnTecho(2,2,2,4));
+console.log("caben:"+maxPanelesEnTecho(2,2,1,15));
+
